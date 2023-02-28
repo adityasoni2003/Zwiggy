32e24c24f9aa77f21556edd2be773e4a07c3b77a
Aditya <adityasoniprofessional@gmail.com>
2023-02-28 11:06:03 +0530

    Got some problems need to solve them

--- src/components/body.js
+++ src/components/body.js
@@ -1,12 +1,45 @@
 
+import { useState } from "react"
+import RestaurantCard from "./restaurant"
+import { restaurantList } from "../config"
+
+const filterData = (search,restaurants)=>{
+    
+
+    const filteredData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+    return filteredData ;
+
+}
+
 const Body = ()=>{
+
+    const [searchVal , setSearchVal] = useState();
+    const [restaurants,setRestaurants] = useState(restaurantList);
+
     return (
         <div className="body">
             <div className="searchBar">
-                <input type="text" name="searchBar" value={"Foobar"} />
-                <button className="searchBtn">Search</button>
+                <input type="text" name="searchBar" value={searchVal} 
+                onChange={(e)=>{
+                    setSearchVal(e.target.value)
+                }}/>
+                <button className="searchBtn"
+                onClick={()=>{
+                    const data = filterData(searchVal,restaurants);
+                    setRestaurants(data);
+
+                }}>Search</button>
             </div>
             <div className="container">
+                {
+                    restaurants.map((restaurant)=>{
+                        return (
+                            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+                        )
+                    })
+                }
 
             </div>
         </div>
